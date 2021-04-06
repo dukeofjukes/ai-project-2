42af2c9c6b7f1940f6629cb56a05964a46205a9f
Brandon Burtchell <brandonburtchell@gmail.com>
2021-04-06 14:54:13 -0500

    resolved errors in minimaxAB()
    utilizing vectors to store paths and successors. algorithm matches that provided in the slides.
    
    next steps: work on the helper functions

--- Project2.cpp
+++ Project2.cpp
@@ -12,9 +12,9 @@ using namespace std;
 
 struct Node {
   int value;
-  int path;
+  vector<Node> path;
 
-  Node(int v, int p) {
+  Node(int v, vector<Node> p) {
     value = v;
     path = p; // FIXME; should this be a list? or just point to next node?
   }
@@ -24,7 +24,7 @@ struct Node {
 Node minimaxAB(int position, int depth, bool player, int useThresh, int passThresh);
 bool deepEnough(int position, int depth);
 int staticEval(int position, bool player);
-Node* moveGen(int position, bool player);
+vector<Node> moveGen(int position, bool player);
 //drawBoard(...);
 
 //TODO: for each group member, define evaluation functions (print tables to a file?)
@@ -33,31 +33,34 @@ int main() {
   return 0;
 }
 
+// useThresh is worst score for MAX (negative val)
+// passThreshh is worst score for MIN (positive val)
 Node minimaxAB(int position, int depth, bool player, int useThresh, int passThresh) {
   if (deepEnough(position, depth)) {
-    // FIXME: need to determine a value for int path ~= NULL, using 0 as placeholder
-    return Node(staticEval(position, player), 0);
+    vector<Node> path; // create a null vector to pass to Node struct
+    return Node(staticEval(position, player), path);
   }
   
-  // generate another player level of the tree
-  // FIXME: should we use the list c++ structure?
-  Node *successors = moveGen(position, player);
+  vector<Node> successors = moveGen(position, player); // generate another level of the tree
 
-  if (successors.empty() == 0) {
-    return Node(staticEval(position, player), 0);
+  if (successors.empty()) {
+    vector<Node> path; // create a null vector to pass to Node struct
+    return Node(staticEval(position, player), path);
   }
   
-  int newValue, bestPath;
+  int newValue;
+  vector<Node> bestPath;
   for (Node succ : successors) {
     Node result_succ = minimaxAB(succ.value, depth + 1, !player, -(passThresh), -(useThresh));
     newValue = -(result_succ.value);
     if (newValue > passThresh) {
       newValue = passThresh;
-      //bestPath = ???;
+      bestPath = result_succ.path;
+      bestPath.insert(bestPath.begin(), succ);
     }
 
     if (passThresh >= useThresh) {
-      return Node(passThresh, bestPath);
+      break;
     }
   }
 
@@ -66,14 +69,14 @@ Node minimaxAB(int position, int depth, bool player, int useThresh, int passThre
 
 bool deepEnough(int position, int depth) {
   // FIXME: determine what value represents a terminal node for position
-  if (depth == 0 /*|| position == terminal node*/) {
+  if (depth == 0 /*|| position == terminal node / game over*/) {
     return true;
   }
   return false;
 }
 
 /*
-  the static evaluation function
+  the static evaluation function, or the "heuristic"
   returns: a number respresenting the goodness of position from the standpoint of the player
 */
 int staticEval(int position, bool player) {
@@ -84,6 +87,8 @@ int staticEval(int position, bool player) {
   the plausible-move generator
   returns: a list of nodes representing moves that can be made
 */
-Node* moveGen(int position, bool player) {
+vector<Node> moveGen(int position, bool player) {
+  vector<Node> successors;
 
+  return successors;
 }
